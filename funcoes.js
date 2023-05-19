170456c6d191d3468dafa755639f53d193a12954
Jayme Holanda <112284700+JaymeHolanda@users.noreply.github.com>
2023-05-19 12:16:34 -0300

    Atualização
    Projeto Iate

--- funcoes.js
+++ funcoes.js
@@ -69,6 +69,8 @@ function validarUsuario(objLoginSenha) {
     });
 
     return retorno;
+
+    console.log(objLoginSenha)
 }
 
 
@@ -90,5 +92,81 @@ function criarObjeto() {
     
     inserirMensagem(obj);
     
+}
+
+function criarLinhasTabela() {
+    var mensagens = obterMensagens();
+    
+    
+    for (var i = 0; i < mensagens.length; i++) {
+        var nome = mensagens[i].nome;
+        var email = mensagens[i].email;
+        var mensagem = mensagens[i].mensagem;
+
+        var linha = document.createElement('tr');
+
+        var tdNome = document.createElement('td');
+        tdNome.innerHTML = nome;
+
+        var tdEmail = document.createElement('td');
+        tdEmail.innerHTML = email;
+    
+        var tdMensagem = document.createElement('td');
+        tdMensagem.innerHTML = mensagem;
+    
+        linha.appendChild(tdNome);
+        linha.appendChild(tdEmail);
+        linha.appendChild(tdMensagem);
+    
+        var tabela = document.getElementById("tabela");
+        tabela.appendChild(linha);
+    }
+    estilizarTabela();
+}
+function estilizarTabela(){
+
+    var linhas = document.getElementsByTagName('tr');
+    var pintar = true;
+
+    for(var i = 1; i<linhas.length; i++){
+
+        if(pintar){
+            linhas[i].setAttribute('style', 'background-color:gray; color:write;')
+            pintar = false;
+        }else {
+            pintar = true;
+        }
+    }
+
+}
+function login() {
+
+
+    var email = document.getElementById("email").value;
+    var senha = document.getElementById("senha").value;
+
+    var objLogin = {email: email, senha: senha};
+    console.log(objLogin);
+    validar();
+
+}
+
+function validar() {
+
+    var dado = {};
+    dado.email = document.getElementById('email').value;
+    dado.senha = document.getElementById('senha').value;
+
+    console.log(dado);
+
+    if (validarUsuario(dado) == true) {
+
+        location.href="mensagens.html";
+    } else {
+        alert('Usuário e senha incorretos!');
+    }
+
+    console.log(validar)
+
 }
 
